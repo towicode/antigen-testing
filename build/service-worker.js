f25b80a5b640cd1c2f61d8f5aebf96455c9c47ae
Todd Wickizer <you@example.com>
2020-08-10 09:57:32 -0700

    add more testing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.caead1bb1a29292a092cda2f689b1c72.js"
+  "/antigen-testing/precache-manifest.91c83b7c44fea5e278e052456deead01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/antigen-testing/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
---
+++ docs/precache-manifest.91c83b7c44fea5e278e052456deead01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f1a328d4e75831807c96b0fb96a5f6",
+    "url": "/antigen-testing/static/media/t3logo.a4f1a328.png"
+  },
+  {
+    "revision": "3547093ecdb1aa13ee48",
+    "url": "/antigen-testing/static/js/runtime~main.3547093e.js"
+  },
+  {
+    "revision": "ce209ed2b47554603d2f",
+    "url": "/antigen-testing/static/js/main.ce209ed2.chunk.js"
+  },
+  {
+    "revision": "5c910550e2dd7b2a1d5d",
+    "url": "/antigen-testing/static/js/1.5c910550.chunk.js"
+  },
+  {
+    "revision": "ce209ed2b47554603d2f",
+    "url": "/antigen-testing/static/css/main.5896640d.chunk.css"
+  },
+  {
+    "revision": "5c910550e2dd7b2a1d5d",
+    "url": "/antigen-testing/static/css/1.1101dc28.chunk.css"
+  },
+  {
+    "revision": "4a0f5a66445e132c86b3dbc29ad36c1a",
+    "url": "/antigen-testing/index.html"
+  }
+];
